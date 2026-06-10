98bdc8cbbb10f79090876b1ee12446bd8d148b13
lamurbob28-prog <lamurbob28@gmail.com>
2026-06-09 22:12:40 -0400

    Add upload settings model

---
+++ dashlander-android/app/src/main/java/com/dashlander/console/UploadSettings.java
@@ -0,0 +1,11 @@
+package com.dashlander.console;
+
+public class UploadSettings {
+    public String username = "BrotherOnGod";
+    public String accountId = "";
+    public String onlineLevelName = "";
+    public boolean unlisted = true;
+    public boolean forceStockSong = false;
+    public String songIdOverride = "";
+    public String audioTrackOverride = "";
+}
