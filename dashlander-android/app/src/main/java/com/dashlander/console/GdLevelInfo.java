2b8d0862b9a4311e8cf3672b227191d779813e68
lamurbob28-prog <lamurbob28@gmail.com>
2026-06-09 22:07:06 -0400

    Add GD level info model

---
+++ dashlander-android/app/src/main/java/com/dashlander/console/GdLevelInfo.java
@@ -0,0 +1,14 @@
+package com.dashlander.console;
+
+public class GdLevelInfo {
+    public String sourceName = "";
+    public String originalLevelId = "";
+    public String levelName = "";
+    public String creator = "";
+    public String songId = "0";
+    public String audioTrack = "0";
+    public String objects = "0";
+    public String levelLength = "0";
+    public int levelStringLength = 0;
+    public String levelStringHash = "";
+}
