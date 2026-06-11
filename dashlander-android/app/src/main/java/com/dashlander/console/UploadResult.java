bffe64be6ac3639dc2e3f6fc5248940e29264bab
lamurbob28-prog <lamurbob28@gmail.com>
2026-06-11 12:09:18 -0400

    Track upload verification status

--- dashlander-android/app/src/main/java/com/dashlander/console/UploadResult.java
+++ dashlander-android/app/src/main/java/com/dashlander/console/UploadResult.java
@@ -7,5 +7,9 @@ public class UploadResult {
     public boolean success = false;
     public String levelId = "";
     public String error = "";
+    public boolean verificationAttempted = false;
+    public boolean verificationFound = false;
+    public String verificationLevelName = "";
+    public String verificationWarning = "";
     public final List<UploadAttempt> attempts = new ArrayList<>();
 }
